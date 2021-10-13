9f6c9852d91497f25d5642c2232e746191e4bfe7
maciejlewicki95 <maciej.lewicki1995@gmail.com>
2021-10-13 19:27:20 +0200

    added some input validations and option for choose a flat floor

--- src/components/FirstStep.jsx
+++ src/components/FirstStep.jsx
@@ -3,9 +3,9 @@ import './test.css'
 
 export const FirstStep = () => {
 
-    const [hasKids, setHasKids] = useState(false);
+    const [hasKids, setHasKids] = useState(null);
     const [age, setAge] = useState(1);
-    const [howManyKids, setHowManyKids] = useState(1);
+    const [howManyKids, setHowManyKids] = useState(null);
     const [kids, setKids] = useState([]);
     const [errorAgeLessOrEqualToZero, setErrorAgeLessOrEqualToZero] = useState("");
     const [errorKidsLessOrEqualToZero, setErrorKidsLessOrEqualToZero] = useState("");
@@ -13,7 +13,15 @@ export const FirstStep = () => {
     const [gender, setGender] = useState("");
     const [houseType, setHouseType] = useState("");
     const [customHouseType, setCustomHouseType] = useState("");
+    const [flatFloor, setFlatFloor] = useState(null);
 
+    useEffect(() => {
+        document.getElementById("children_question_no").addEventListener("change", test);
+        return () => {
+            document.getElementById("children_question_no").addEventListener("change", test);
+        }
+    }, []);
+    
     function validateAgeOnChange(event) {
         if (event.target.value == "") {
             setAge(event.target.value);
@@ -29,12 +37,13 @@ export const FirstStep = () => {
     }
 
     function validateCountOfKids(event) {
+        console.log(event.target.value)
         if (event.target.value == "") {
             setHowManyKids(event.target.value);
         }
         else if (event.target.value <= 0) {
             setErrorKidsLessOrEqualToZero('Zaznaczono wcześniej, że posiada Pan/i dzieci. Proszę wskazać poprawną liczbę lub zmienić odpowiedź powyżej z "tak" na "nie".');
-            setHowManyKids(1);
+            setHowManyKids(event.target.value);
         }
         else {
             setErrorKidsLessOrEqualToZero("")
@@ -42,25 +51,25 @@ export const FirstStep = () => {
         }
     }
 
-    function enableCustomAnswer(){
+    function enableCustomAnswer() {
         let customAnswer = document.getElementById("custom_answer_for_house_type");
         customAnswer.disabled = false;
     }
 
-    function disableCustomAnswer(){
+    function disableCustomAnswer() {
         let customAnswer = document.getElementById("custom_answer_for_house_type");
         customAnswer.disabled = true;
     }
 
-    function isValidCharacter(event){
+    function isValidCharacter(event) {
         var index = event.target.value.length - 1
         console.log(index);
         console.log(event.target.value.charCodeAt(index));
-        if ((event.target.value.charCodeAt(index) >= 65 && event.target.value.charCodeAt(index) <= 90) || 
-        (event.target.value.charCodeAt(index) >= 97 && event.target.value.charCodeAt(index) <= 122) || 
-        (event.target.value.charCodeAt(index) == 32) || 
-        (event.target.value.charCodeAt(index) >= 44 && event.target.value.charCodeAt(index) <= 46) || 
-        (event.target.value.length <= 0)) {
+        if ((event.target.value.charCodeAt(index) >= 65 && event.target.value.charCodeAt(index) <= 90) ||
+            (event.target.value.charCodeAt(index) >= 97 && event.target.value.charCodeAt(index) <= 122) ||
+            (event.target.value.charCodeAt(index) == 32) ||
+            (event.target.value.charCodeAt(index) >= 44 && event.target.value.charCodeAt(index) <= 46) ||
+            (event.target.value.length <= 0)) {
             setErrorCustomHouseType("");
             setCustomHouseType(event.target.value);
             setHouseType(event.target.value);
@@ -70,13 +79,40 @@ export const FirstStep = () => {
         }
     }
 
-    function changeHouseType(houseType){
+    function changeHouseType(houseType) {
         disableCustomAnswer();
         setCustomHouseType("");
         setHouseType(houseType);
     }
 
+    const imageHandler = event => {
+        let file = event.target.files[0];
+        console.log(file);
+        // console.log(URL.createObjectURL(file));
+    };
+
+    function test(){
+        setErrorKidsLessOrEqualToZero("");
+    }
+
+    function handleHasChildren(event){
+        setHasKids(true);
+        document.getElementById("count_of_kids_container").style.display = "inline";
+    }
+
+    function handleDontHaveChildren(event){
+        setHasKids(false);
+        document.getElementById("count_of_kids_container").style.display = "none";
+    }
+
+    function validateSetFloor(event){
+        console.log(event.target.value);
+        setFlatFloor(event.target.value);
+    }
+
     let hasGender = (gender == "male" || gender == "female")
+    let noErrors = (errorAgeLessOrEqualToZero == "" && errorKidsLessOrEqualToZero == "")
+    let allQuestionsHaveAnswer = (gender != "" && houseType != "" && hasKids != null)
 
 
     return (
@@ -111,25 +147,22 @@ export const FirstStep = () => {
                             {gender == "male" && (<span>Czy ma Pan dzieci?</span>)}
                         </label>
                         <div className="answers">
-                            <input className='picking_input' name='children_question' id='children_question' type='radio' onChange={event => setHasKids(true)} />
+                            <input className='picking_input' name='children_question' id='children_question_yes' type='radio' onChange={event => handleHasChildren(event)} />
                             Tak
                             <br />
-                            <input className='picking_input' name='children_question' id='children_question' type='radio' onChange={event => setHasKids(false)} />
+                            <input className='picking_input' name='children_question' id='children_question_no' type='radio' onChange={event => handleDontHaveChildren(event)} />
                             Nie
                         </div>
                     </div>
-                    {hasKids && (
-                        <><div className="input-group">
-                            <label htmlFor="count_of_kids">Proszę wskazać ilość dzieci:</label>
-                            <input className='default' name='count_of_kids' id='count_of_kids' type="number" value={howManyKids} onChange={event => validateCountOfKids(event)} />
-                        </div>
-                            {errorKidsLessOrEqualToZero != "" && (
-                                <div className="error">
-                                    {errorKidsLessOrEqualToZero}
-                                </div>
-                            )}
-                        </>
-                    )}
+                    <div className="input-group" id="count_of_kids_container" style={{display: "none"}}>
+                        <label htmlFor="count_of_kids">Proszę wskazać ilość dzieci:</label>
+                        <input className='default' name='count_of_kids' id='count_of_kids' type="number" min="0" step="1" value={howManyKids} onChange={event => validateCountOfKids(event)} />
+                    </div>
+                    {errorKidsLessOrEqualToZero != "" && (
+                        <div className="error">
+                            {errorKidsLessOrEqualToZero}
+                            </div>
+                        )}
                     <h2>Informacje o mieszkaniu</h2>
                     <div className="input-choice">
                         <label htmlFor='house_type_question'>Typ mieszkania:</label>
@@ -141,9 +174,17 @@ export const FirstStep = () => {
                             Dom jednorodzinny
                             <br />
                             <input className='picking_input' name='house_type_question' id='house_type_question' type='radio' onChange={event => enableCustomAnswer()} />
-                            <input type='text' id="custom_answer_for_house_type" style={{height: "21px", width: "20vw"}} disabled placeholder="inne..." value={customHouseType} onChange={event => isValidCharacter(event)} />
+                            <input type='text' id="custom_answer_for_house_type" style={{ height: "21px", width: "20vw" }} disabled placeholder="inne..." value={customHouseType} onChange={event => isValidCharacter(event)} />
                         </div>
                     </div>
+                    {houseType == "flat" && (
+                        <>
+                            <div className="input-group">
+                                <label htmlFor="flat_floor">Proszę wskazać piętro na którym znajduje się mieszkanie (parter=0):</label>
+                                <input className='default' name='flat_floor' id='flat_floor' type="number" min="0" step="1" value={flatFloor} onChange={event => validateSetFloor(event)} />
+                            </div>
+                        </>
+                    )}
                     {errorCustomHouseType != "" && (
                         <div className="error">
                             {errorCustomHouseType}
@@ -151,11 +192,19 @@ export const FirstStep = () => {
                     )}
                     <div className="input-group">
                         <label htmlFor='files'>Dodaj zdjęcia mieszkania:</label>
-                        <input className='inputfile' id='files' type="file" multiple data-multiple-caption="{count} files selected" />
+                        <input className='inputfile' id='files' type="file" multiple onChange={imageHandler} />
                     </div>
-                    <button className="submit-btn" type='submit'>Prześlij dane</button>
+                {noErrors && allQuestionsHaveAnswer &&
+                    (<button className="submit-btn" type='submit' id="submit_button_enabled">Prześlij dane</button>)}
+                {(!noErrors || !allQuestionsHaveAnswer) &&
+                    (<button className="submit-btn" type='submit' id="submit_button_disabled" title="Nie możesz przesłać danych, ponieważ formularz zawiera błędy lub niektóre pytania nie mają odpowiedzi" disabled >Prześlij dane
+                    </button>)}
+                {!allQuestionsHaveAnswer &&
+                    (<div className="error">Nie możesz przesłać danych, ponieważ formularz zawiera pytania bez odpowiedzi!</div>)}
+                {!noErrors &&
+                    (<div className="error">Nie możesz przesłać danych, ponieważ formularz zawiera błędy!</div>)}
                 </form>
-                
+
             </div>
 
         </>
