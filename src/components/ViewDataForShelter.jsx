e0f0de113a1fe1d5320a89ed17335c24da298af2
maciejlewicki95 <maciej.lewicki1995@gmail.com>
2021-10-25 17:58:25 +0200

    finished view data for shelter

--- src/components/ViewDataForShelter.jsx
+++ src/components/ViewDataForShelter.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-// import './displayImages.css'
 import './style.css'
-// const Lightbox = require('react-awesome-lightbox'); 
 
 
 
@@ -22,27 +20,70 @@ export const ViewDataForShelter = () => {
 
     useEffect(() => {
 
-        const url = 'http://localhost:8080/application-data/7';
+        const url = 'http://localhost:8080/application-data/5';
         fetch(url)
             .then(data => data.json())
             .then(response => setAppState(response));
     })
 
+    function ageConverter(ageInMonths){
+        let months = ageInMonths % 12
+        let years = (ageInMonths - months) / 12
+        let correctFormForMonths = correctMonthEnding(months)
+        let correctFormForYears = correctYearEnding(years)
+        if (years <= 0) {
+            return `${months} ${correctFormForMonths}`
+        }
+        else {
+            return `${years} ${correctFormForYears} i ${months} ${correctFormForMonths}`
+        }
+    }
+
+    function correctMonthEnding(ageInMonths){
+        if (ageInMonths == 1) return "miesiąc"
+        else if (ageInMonths == 0 || ageInMonths > 4) return "miesięcy"
+        else return "miesiące"
+    }
+
+    function correctYearEnding(ageInYears){
+        if (ageInYears == 1) return "rok"
+        else if (ageInYears > 4) return "lat"
+        else return "lata"
+    }
+
     return (
         <>
             <div className="container">
-                {appState != null && (<><div>{appState.id}</div><div>{appState.has_kids}</div>
-                    <div>{appState.gender}</div>
-                    <div>{images.length}</div></>)}
-                <div>
-                    <div className="gallery">
-                        {images.map(image => {
-                            return (
-                                <a href={image} data-lightbox="test"> <img src={image} /></a>
-                            )
-                        })}
+                <h1>Dane o osobie składającej wniosek</h1>
+                {appState != null && (<><div className="data-container">
+                    <div className="data-container-element"><span>Wiek: </span>{appState.age}</div>
+                    <div className="data-container-element"><span>Płeć: </span>{appState.gender == "male" ? ("mężczyzna") : ("kobieta")}</div>
+                    <div className="data-container-element"><span>Liczba dzieci: </span>{appState.howManyKids}</div>
+                    {appState.howManyKids > 0 && (<div className="data-container-element"><span>Wiek najmłodszego dziecka: </span>{ageConverter(appState.ageOfYoungestKid)}</div>
+                                        )}
                     </div>
+                    <h1>Dane o miejscu zamieszkania</h1>
+                    <div className="data-container">
+                    <div className="data-container-element"><span>Typ: </span>
+                        {appState.houseType == "single family house" && ("dom jednorodzinny")}
+                        {appState.houseType == "flat" && ("mieszkanie")}
+                        {appState.houseType != "single family house" && appState.houseType != "flat" && (appState.houseType)}
+                    </div>
+                    {appState.houseType == "flat" && (<div className="data-container-element"><span>Piętro: </span>{appState.flatFloor}</div>)}
+                    <div className="data-container-element"><span>Powierzchnia: </span><span>{appState.houseArea}m<sup>2</sup></span></div>
+                    </div>
+                    <h1>Załączone zdjęcia</h1>
+                    <div className="gallery">
+                    {images.map(image => {
+                        return (
+                            <div className="single-image">
+                                <a href={image}><img src={image} /></a>
+                            </div>
+                        )
+                    })}
                 </div>
+                    </>)}
+
             </div>
 
         </>
